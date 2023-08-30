41fcc1c457331e28da0f9c69025392c195eed5d8
Sergiy Nikolaev <122089422+fsdev3@users.noreply.github.com>
2023-08-30 13:56:00 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { App } from 'components/App';
 import { store, persistor } from 'redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
